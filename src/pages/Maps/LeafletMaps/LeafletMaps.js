399523169b1408f7eb8f4879c6e12620506e0cad
Sazzad <sazzad47.ju@gmail.com>
2023-03-27 17:47:22 -0700

    make it better

--- src/pages/Maps/LeafletMaps/LeafletMaps.js
+++ src/pages/Maps/LeafletMaps/LeafletMaps.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Card, CardBody, CardHeader, Col, Container, Row } from 'reactstrap';
+import React from "react";
+import { Card, CardBody, CardHeader, Col, Container, Row } from "reactstrap";
 
 //Import Breadcrumb
-import BreadCrumb from '../../../Components/Common/BreadCrumb';
+import BreadCrumb from "../../../Components/Common/BreadCrumb";
 
 //Import maps
 import SimpleMap from "./MapsLeaflet/SimpleMap";
@@ -13,100 +13,106 @@ import LayerGroup from "./MapsLeaflet/LayerGroup";
 import MapLayerControl from "./MapsLeaflet/MapLayerControl";
 
 const LeafletMaps = () => {
-     document.title="Leaflet Maps | Velzon - React Admin & Dashboard Template";
-    return (
-        <React.Fragment>
-            <div className="page-content">
-                <Container fluid>
-                    <BreadCrumb title="Leaflet" pageTitle="Maps" />
-                    <Row>
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Example</h4>
-                                </CardHeader>
+  document.title = "Leaflet Maps | Velzon - React Admin & Dashboard Template";
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <Container fluid>
+          <BreadCrumb title="Leaflet" pageTitle="Maps" />
+          <Row>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">Example</h4>
+                </CardHeader>
 
-                                <CardBody>
-                                    <div id="leaflet-map" className="leaflet-map">
-                                        <SimpleMap />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Markers, Circles and Polygons</h4>
-                                </CardHeader>
+                <CardBody>
+                  <div id="leaflet-map" className="leaflet-map">
+                    <SimpleMap />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">
+                    Markers, Circles and Polygons
+                  </h4>
+                </CardHeader>
 
-                                <CardBody>
-                                    <div id="leaflet-map-marker" className="leaflet-map">
-                                        <MapVectorLayers />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Working with Popups</h4>
-                                </CardHeader>
-                                <CardBody>
-                                    <div id="leaflet-map-popup" className="leaflet-map">
-                                        <MapWithPopup />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
+                <CardBody>
+                  <div id="leaflet-map-marker" className="leaflet-map">
+                    <MapVectorLayers />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">Working with Popups</h4>
+                </CardHeader>
+                <CardBody>
+                  <div id="leaflet-map-popup" className="leaflet-map">
+                    <MapWithPopup />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
 
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Markers with Custom Icons</h4>
-                                </CardHeader>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">Markers with Custom Icons</h4>
+                </CardHeader>
 
-                                <CardBody>
-                                    <div id="leaflet-map-custom-icons" className="leaflet-map">
-                                        <MapMarkerCustomIcons />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Interactive Choropleth Map</h4>
-                                </CardHeader>
+                <CardBody>
+                  <div id="leaflet-map-custom-icons" className="leaflet-map">
+                    <MapMarkerCustomIcons />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">
+                    Interactive Choropleth Map
+                  </h4>
+                </CardHeader>
 
-                                <CardBody>
-                                    <div id="leaflet-map-interactive-map" className="leaflet-map">
-                                        <LayerGroup />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col lg={6}>
-                            <Card>
-                                <CardHeader>
-                                    <h4 className="card-title mb-0">Layer Groups and Layers Control</h4>
-                                </CardHeader>
+                <CardBody>
+                  <div id="leaflet-map-interactive-map" className="leaflet-map">
+                    <LayerGroup />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col lg={6}>
+              <Card>
+                <CardHeader>
+                  <h4 className="card-title mb-0">
+                    Layer Groups and Layers Control
+                  </h4>
+                </CardHeader>
 
-                                <CardBody>
-                                    <div id="leaflet-map-group-control" className="leaflet-map">
-                                        <MapLayerControl />
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </React.Fragment>
-    );
+                <CardBody>
+                  <div id="leaflet-map-group-control" className="leaflet-map">
+                    <MapLayerControl />
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default LeafletMaps;
